9874402011e48a244b9315bcbe80485bb9af650c
Your Nameeli <elighidiu@yahoo.com>
2021-05-10 15:40:13 +0200

    displaying forecast info

--- main.js
+++ main.js
@@ -13,11 +13,17 @@
     //using Axios library to get the current weather 
     async function weatherData(city) {
         try {
-        const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=5&appid=f553ea1ef891582d4233868a79cc11e9`);
+        const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=6&appid=f553ea1ef891582d4233868a79cc11e9`); // added units as metrics to display the temperature in Celsius as per api documentation
+       
+        response.data.list.forEach((element, index) => {
+            console.log(element.main.temp);
+           // displayForecast(city, element.main.temp, index);
+            displayForecast(city, element, index);
+        });
         
-        document.getElementsByTagName("p")[0].innerHTML=`There are ${response.data.list[0].main.temp}&#8451; in ${city}`;
+        
         console.log(response);
-        
+      
 
         } catch (error) {
         console.error(error);
@@ -29,5 +35,22 @@
         city = e.target.value;
         return city;
     };
+
+    function displayForecast(location, weatherDetails, index){
+       
+        //document.getElementsByTagName("p")[index].innerHTML=`There are ${temperature}&#8451; in ${location}`;
+        let card = document.getElementsByClassName("card")[index];
+
+        let tempText = document.createElement("p");
+        let weatherText = document.createElement("p");
+        
+        //console.log(card);
+        tempText.innerHTML = `There are ${weatherDetails.main.temp}&#8451; in ${location}`;
+        weatherText.innerHTML = `There are ${weatherDetails.weather[0].main} : ${weatherDetails.weather[0].description} in ${location}`;
+
+        card.appendChild(tempText);
+        card.appendChild(weatherText);
+        
+    }
 
 })();
