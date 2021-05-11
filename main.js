c761fe13f7b7b241a7eb986e0a06c09a70dd6173
Your Nameeli <elighidiu@yahoo.com>
2021-05-11 12:52:10 +0200

    https api link

--- main.js
+++ main.js
@@ -13,7 +13,7 @@
     //using Axios library to get the current weather 
     async function weatherData(city) {
         try {
-        const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=f553ea1ef891582d4233868a79cc11e9`); // added units as metrics to display the temperature in Celsius as per api documentation
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=f553ea1ef891582d4233868a79cc11e9`); // added units as metrics to display the temperature in Celsius as per api documentation
 
         console.log(response);
             
